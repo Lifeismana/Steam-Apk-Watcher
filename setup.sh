6f342a2d955c0230c07d0a550eeea2ac2f06daee
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2024-07-10 12:01:54 +0200

    fix git ids

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+[ -n "$GIT_NAME" ] && git config --global user.name "$GIT_NAME"
+[ -n "$GIT_EMAIL" ] && git config --global user.email "$GIT_EMAIL"
 git config --global --add safe.directory $GITHUB_WORKSPACE
 cd $GITHUB_WORKSPACE && ./script.sh
