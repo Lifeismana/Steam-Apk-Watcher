8bd77cabfa2b6316a71b392866bccdb11e078a5b
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2025-07-31 00:24:54 +0200

    com.valvesoftware.steamlinkvr Version 2.0.14 - 5001503 -> 5001546 | 4 files | com.valvesoftware.steamlinkvr.apk.jadx.txt, com.valvesoftware.steamlinkvr.apk.txt, AndroidManifest.xml, BuildConfig.java

--- com.valvesoftware.steamlinkvr/sources/com/valvesoftware/steamlink/BuildConfig.java
+++ com.valvesoftware.steamlinkvr/sources/com/valvesoftware/steamlink/BuildConfig.java
@@ -6,6 +6,6 @@ public final class BuildConfig {
     public static final String BUILD_TYPE = "release";
     public static final boolean DEBUG = false;
     public static final String FLAVOR = "steamlinkvr";
-    public static final int VERSION_CODE = 5001503;
+    public static final int VERSION_CODE = 5001546;
     public static final String VERSION_NAME = "2.0.14";
 }
